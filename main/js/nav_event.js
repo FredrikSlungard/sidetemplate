871917f724b106097f435c6e08e7d244bf045de0
Fredrik Slungård <Fredrik.slungard@outlook.com>
2018-10-18 11:48:35 +0200

    La til scrolling når du trykker på navigasjonen

---
+++ main/js/nav_event.js
@@ -0,0 +1,37 @@
+
+// Går til overskriften i dokumentet når noen tykker på overskriften i navigasjonen
+// Hvis det er flere treff går den til den første overskriften
+$('.nav_clickable').on('click', function () {
+  let Overskrifter = $('h1,h2');
+  let Søk_Etter = $(this).text();
+  let Destinasjon = 0;
+
+  const windowHeight = $(window).height();
+
+  // Søker etter overskriften og filterer ut verdiene som ikke treffer søkeverdien
+  let Result = Overskrifter.filter(function () {
+    return $(this).text() === Søk_Etter;
+  });
+
+  // Velger destinasjon utifra antall treff, ved flere treff velges den øverste
+  if (Result.length === 1) {
+    Destinasjon = $(Result).offset().top;
+  }
+  else {
+    let Første_Overskrift = Result[0];
+    Destinasjon = $(Første_Overskrift).offset().top;
+  };
+
+  // Gjør at den scroller til midten av siden hvis overskriften er lavere enn
+  // høyden på vinduet
+  if (Destinasjon < windowHeight) {
+    Destinasjon -= (windowHeight / 2);
+  };
+
+
+  // Scroller til destinasjonen
+  $('html, body').animate({
+    scrollTop: (Destinasjon)
+  }, 700);
+
+}); 
--- main/js/navigasjon.js
+++ main/js/navigasjon.js
@@ -1,48 +1,46 @@
-'use strict'
 /* Legger til overskrifter i navigasjonen til venstre, med referanse til overskriften hvis den har en ID. Hvis ikke søkes det etter overskriften når brukeren klikker på overskriften
 
 h1 Legges til som navmenu-brand 
 h2 legges til som liste under h1 */
 Legg_Til_Overskrift = () => {
-
+  'use strict'
+  
   let destinasjon = $('#nav_innhold');
   let Overskrifter = $('h1,h2');
   let innhold = '';
 
-  destinasjon.empty();
+  destinasjon.empty(); // Fjerner gammelt innhold i div, f.eks. testeverdier etc.
+
+  // Gå gjennom alle overskriftene og legg til i overskriftene
   $.each(Overskrifter, function (index, value) {
+    
+    // Lager h2 elementer som et liste element med href
+    if ($(value).is('h2')) {
+      innhold += '<li><a class="nav_clickable" href="#">' + $(value).text() + '</li></a>';
 
-    innhold += '<a class="nav_Clickable" href="#">' + $(this).text() + '</a>';
-    //console.log($(this).url);
+    }
+    // Start en ny liste og legg til h1 som brand
+    else if ($(value).is('h1')) {
 
+      if (index === 0) {
+        innhold += '<a class="navmenu-brand nav_clickable" href="#">' + $(value).text() + '</a>';
+      }
+      // Avslutter den gamle overskriften
+      else {
+        innhold += '</ul>';
+        innhold += '<a class="navmenu-brand nav_clickable" href="#">' + $(value).text() + '</a>';
+      };
+
+      // Starter en ny liste under hovedoverskriften
+      innhold += '<ul class="navmenu-nav">';
+
+    };
   });
+
+  // Avslutt den siste listen
+  innhold += '</ul>';
 
   $(innhold).insertAfter($(destinasjon));
 };
 
 Legg_Til_Overskrift();
-
-
-/* // Går til overskriften når noen tykker på overskriften i navigasjonen
-$('.nav_Clickable').on('click', function () {
-  let Overskrifter = $('h2');
-  let Søk_Etter = this.text;
-
-  // Søker etter overskriften og filterer ut verdiene som ikke treffer søkeverdien
-  let Result = $('h2').filter(function () {
-    return $(this).text() === Søk_Etter;
-  });
-
-  if (Result.length === 1) {
-
-    $('html, body').animate({
-      scrollTop: (Result.offset().top)
-    }, 300);
-  }
-  else {
-    /* let Result2 = Result[0];
-    console.log(Result2.position().top);
-  };
-
-}); */
-
